9fd542dd1dbefe86907525f1fa2b52851de34f0f
Dragon Lin <Dragon Lin>
2017-05-18 01:48:59 +0800

    add to_update_sql wi bind=False

--- df2sql.py
+++ df2sql.py
@@ -18,3 +18,33 @@ def to_insert_sql(df, cols, table, bind=False):
             value_str = value_str.replace("(", '')
             value_str = value_str.replace(")", '')
             print(insert_sql.format(table=table, column=cols_str, value=value_str))
+
+
+def to_update_sql(df, pri_cols, cols, table, bind=False):
+    update_sql = """
+    UPDATE {table}
+       SET {cols}
+     WHERE 1=1
+     {pri_key}"""
+
+    for row in df[cols].itertuples(index=False):
+        row_dict = row._asdict()
+        pri_cols = {k: v for k, v in row_dict.items() if k in pri_cols}
+        set_cols = {k: v for k, v in row_dict.items() if k not in pri_cols}
+
+        pri_key_str = ''
+        for idx, pri_data in enumerate(pri_cols.items()):
+            pri_col = pri_data[0]
+            pri_val = "'{}'".format(pri_data[1]) if isinstance(pri_data[1], str) else pri_data[1]
+            partial_key = '  AND {} = {}'.format(pri_col, pri_val)
+            pri_key_str += partial_key
+            pri_key_str += '\n     ' if idx + 1 != len(pri_cols) else ''
+
+        set_val_str = ''
+        for idx, set_data in enumerate(set_cols.items()):
+            set_col = set_data[0]
+            set_value = "'{}'".format(set_data[1]) if isinstance(set_data[1], str) else set_data[1]
+            partial_set = '{} = {}'.format(set_col, set_value)
+            set_val_str += partial_set
+            set_val_str += ',\n           ' if idx + 1 != len(set_cols) else ''
+        print(update_sql.format(table=table, cols=set_val_str, pri_key=pri_key_str))
--- main.py
+++ main.py
@@ -8,22 +8,26 @@ def main():
     feature['NAME'] = ['Tom', 'Jack', 'Amy', 'Alice']
     feature['AGE'] = [20, 24, 23, 21]
     feature['GENDER'] = ['M', 'M', 'F', 'F']
-    col_list = ['NAME', 'AGE', 'GENDER']
+    feature['TAKE'] = ['Y', 'Y', 'N', 'N']
+    col_list = ['NAME', 'AGE', 'GENDER', 'TAKE']
 
     df = pd.DataFrame(feature, columns=col_list)
     print(df)
 
     # generate insert sql
-    select_cols = ['NAME', 'AGE', 'GENDER']
+    select_cols = ['NAME', 'AGE', 'GENDER', 'TAKE']
     table_name = 'FRIEND'
 
-    from df2sql import to_insert_sql
-    to_insert_sql(df, select_cols, table_name, bind=False)
-    to_insert_sql(df, select_cols, table_name, bind=True)
+    from df2sql import to_insert_sql, to_update_sql
+    # to_insert_sql(df, select_cols, table_name, bind=False)
+    # to_insert_sql(df, select_cols, table_name, bind=True)
+
+    pri_key = ['NAME', 'AGE']
+    select_cols = ['NAME', 'AGE', 'GENDER', 'TAKE']
+    to_update_sql(df, pri_key, select_cols, table_name, bind=False)
 
     # cols_str = reduce(lambda m, n: m + ',' + n, select_cols)
-    cols_str = ', '.join(select_cols)
-    bind_val_str = ', '.join([':' + x for x in select_cols])
+    pass
 
 
 if __name__ == '__main__':
