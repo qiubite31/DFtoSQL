7e18cdf99fd54b220ff8556a45540d2c9afdb630
Drake Lin <qiubite31@hotmail.com>
2018-10-08 19:22:53 +0800

    update handling to_update_sql nan / NaT

--- df2sql.py
+++ df2sql.py
@@ -56,7 +56,7 @@ def to_update_sql(df, pri_cols, cols, table, bind=False, chunksize=100):
        SET {cols}
      WHERE 1=1
      {pri_key};"""
-    update_sql = """    BEGIN{all_update}
+    update_sql = """        BEGIN{all_update}
     END;"""
     all_update = ''
     for idx, row in enumerate(df[cols].itertuples(index=False)):
@@ -64,25 +64,42 @@ def to_update_sql(df, pri_cols, cols, table, bind=False, chunksize=100):
         pri_cols = {k: v for k, v in row_dict.items() if k in pri_cols}
         set_cols = {k: v for k, v in row_dict.items() if k not in pri_cols}
 
+        # 拆成set要接的str和where要接的str
         pri_key_str = ''
         for pri_idx, pri_data in enumerate(pri_cols.items()):
             pri_col = pri_data[0]
             pri_val = "'{}'".format(pri_data[1]) if isinstance(pri_data[1], str) else pri_data[1]
             select_pri_val = ':' + pri_col if bind else pri_val
-            partial_key = '  AND {} = {}'.format(pri_col, select_pri_val)
-            # 把nan取代成NULL
-            partial_key = partial_key.replace('nan', 'NULL')
+            # 如果是Timestamp格式，要用repr轉出並取掉左右刮號
+            if 'TIMESTAMP' in repr(select_pri_val).upper():
+                partial_key = '  AND {} = {}'.format(pri_col, repr(select_pri_val).replace('(', '').replace(')', ''))
+            else:
+                partial_key = '  AND {} = {}'.format(pri_col, select_pri_val)
+            # 處理nan和NaT的NULL
+            if 'nan' in partial_key:
+                partial_key = partial_key.replace('nan', 'NULL')
+                partial_key = partial_key.replace('= NULL', 'IS NULL')
+            if 'NaT' in partial_key:
+                partial_key = partial_key.replace('NaT', 'NULL')
+                partial_key = partial_key.replace('= NULL', 'IS NULL')
             pri_key_str += partial_key
             pri_key_str += '\n     ' if pri_idx + 1 != len(pri_cols) else ''
 
         set_val_str = ''
         for set_idx, set_data in enumerate(set_cols.items()):
             set_col = set_data[0]
-            set_val = "'{}'".format(set_data[1]) if isinstance(set_data[1], str) else set_data[1]
-            select_set_val = ':' + set_col if bind else set_val
-            partial_set = '{} = {}'.format(set_col, select_set_val)
-            # 把nan取代成NULL
-            partial_set = partial_set.replace('nan', 'NULL')
+            set_val = "'{}'".format(set_data[1].replace("'", "''")) if isinstance(set_data[1], str) else set_data[1]
+            select_set_val = ':' + set_col if params else set_val
+            # 如果是Timestamp格式，要用repr轉出並取掉左右刮號
+            if 'TIMESTAMP' in repr(select_set_val).upper():
+                partial_set = '{} = {}'.format(set_col, repr(select_set_val).replace('(', '').replace(')', ''))
+            else:
+                partial_set = '{} = {}'.format(set_col, select_set_val)
+
+            # 處理nan和NaT的NULL
+            if 'nan' in partial_set or 'NaT' in partial_set:
+                partial_set = partial_set.replace('nan', 'NULL')
+                partial_set = partial_set.replace('NaT', 'NULL')
             set_val_str += partial_set
             set_val_str += ',\n           ' if set_idx + 1 != len(set_cols) else ''
 
@@ -98,7 +115,8 @@ def to_update_sql(df, pri_cols, cols, table, bind=False, chunksize=100):
         else:
             all_update += template_sql.format(table=table, cols=set_val_str, pri_key=pri_key_str)
             if (idx+1) % chunksize == 0 or idx == len(df.index)-1:
-                print(update_sql.format(all_update=all_update))
+                update_sql_str = update_sql.format(all_update=all_update)
+                print(update_sql_str)
                 all_update = ''
 
  def _print_sql(self, sql, params):
